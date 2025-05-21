0fa3460f4bf8bb45fcdb620d4542ddb851d1e5d1
tec.ingdesarrollo <tec.ingdesarrollo@TEC-DES-007.usbbog.edu.co>
2025-05-21 16:38:01 -0500

    Habilitar Framework

---
+++ src/main/java/com/senaproyecto/proyecto/sena/controller/Login.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.senaproyecto.proyecto.sena.controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author tec.ingdesarrollo
+ */
+@Named(value = "login")
+@SessionScoped
+public class Login implements Serializable {
+
+    /**
+     * Creates a new instance of Login
+     */
+    public Login() {
+    }
+    
+}
